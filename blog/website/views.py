efd50bf7e31b20f0577b05c85b89bd500038f2ea
LokusS <willian468@gmail.com>
2020-01-09 16:54:27 -0300

    Atualização

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_blog(request):
+   lista = [
+       'Django', 'Python', 'Git', 'Html',
+       'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
+       'Systemctl'
+    ]
+   data = {'name':'Curso de Django 3', 'lista_tecnologias': lista}
+   return render(request,'index.html', data)
