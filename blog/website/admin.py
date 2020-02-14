7903428377d053c2cfb44bc918918710890a6824
LokusS <willian468@gmail.com>
2020-02-14 11:53:35 -0300

    'atualizando'

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'sub_title']
+    list_display = [
+        'title', 'sub_title', 
+        'categories', 'deleted'
+        ]
     search_fields = ['title', 'sub_title']
-    fields = ('title', 'sub_title')
+    #fields = ('title', 'sub_title')
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=False)
 
 admin.site.register(Post, PostAdmin)
 
