c7eb6f238d0f0bd4674f6b744e0ac6de22758869
LokusS <willian468@gmail.com>
2020-02-21 11:02:15 -0300

    'Atualização'

--- blog/website/models.py
+++ blog/website/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
         choices=Categorias.choices,
         default=Categorias.GR,
     )
-
     deleted = models.BooleanField(default=True)
+    imagem = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
